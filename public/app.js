44e64f3d0846b2371dc8b438aa4ae914ef22d1ce
Criogen11 <tsv1985@bk.ru>
2017-11-01 20:21:30 +0300

    Update add_to_cart

--- public/app.js
+++ public/app.js
@@ -1,16 +1,10 @@
-$(document).ready(function(){
-	
-	$('.alert_btn').click(function(){	
-		var x = window.localStorage.getItem('bbb');	
-		x = x*1 + 1;
-		window.localStorage.setItem('bbb', x);
-		alert(x);
-	});
+function add_to_cart(id) {
+	var key = 'product_' + id;
+	var x = window.localStorage.getItem(key);
+	x = x*1 + 1;
+	window.localStorage.setItem(key, x);
+	console.log(x);
+}
 
-	$('.alert_del').click(function(){
-		var x = window.localStorage.getItem('bbb');	
-		window.localStorage.setItem('bbb', 0);
-	});
-	
-});
+
 
