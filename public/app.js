7c790307d07763a6d41072e83b5facf41019608b
Criogen11 <tsv1985@bk.ru>
2017-11-06 11:36:40 +0300

    Orders input

--- public/app.js
+++ public/app.js
@@ -3,7 +3,7 @@ function add_to_cart(id) {
 	var x = window.localStorage.getItem(key);
 	x = x*1 + 1;
 	window.localStorage.setItem(key, x);
-	console.log(x);
+	update_orders_input();
 } 
 
 function cart_get_number_of_items() {
@@ -18,6 +18,25 @@ function cart_get_number_of_items() {
 
 	}
 	return cnt;
+}
+
+function update_orders_input() {
+	var orders = cart_get_orders();
+	$('#orders_input').val(orders);
+}
+
+function cart_get_orders() {
+	var orders = '';
+	for(var i = 0; i < window.localStorage.length; i++) {
+		var key = window.localStorage.key(i);
+		var value = window.localStorage.getItem(key);
+		if(key.indexOf('product_') == 0)
+		{
+			orders = orders + key + '=' + value + ',';
+		}
+
+	}
+	return orders;
 }
 
 
